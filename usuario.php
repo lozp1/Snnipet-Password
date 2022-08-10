a6e3521b6ebd6cab418f2c22425f30ec4ad80824
franco <91517547+lozp1@users.noreply.github.com>
2022-08-09 21:50:48 -0600

    Add files via upload

---
+++ usuario.php
@@ -0,0 +1,18 @@
+<?php
+
+// Importar Conexión
+
+require 'includes/config/database.php';
+$db = conectarDB();
+
+// Crear email y password
+$email = "francolpz001@gmail.com";
+$password = "123456";
+$passwordHash = password_hash($password, PASSWORD_DEFAULT);
+
+// Consulta para crear el usuario
+$query = " INSERT INTO usuarios (email, password) VALUES ( '${email}', '${passwordHash}'); ";
+//echo $query;
+
+// Agregar usuario en db
+mysqli_query($db, $query);
